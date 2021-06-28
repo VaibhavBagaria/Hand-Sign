eceac8a3758439882d4023bf24e80ab12c5f3fed
VaibhavBagaria <76587881+VaibhavBagaria@users.noreply.github.com>
2021-06-28 15:45:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ console.log("ml5 version:", ml5.version);
 
 classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/TYd2yVjNA/model.json", modelLoaded)
 
-function model_Loaded() {
+function modelLoaded() {
     console.log("model loaded!")
 }
 
@@ -33,4 +33,61 @@ function speak() {
     speak_data2 = "and The Second Prediction Is " + Prediction_2;
     utterthis = new SpeechSynthesisUtterance(speak_data1 + speak_data2)
     synth.speak(utterthis)
+}
+
+
+function Check() {
+    img=document.getElementById("captured_image")
+    classifier.classify(img,Got_Result);
+
+}
+
+function Got_Result(error,results) {
+    if(error){
+        console.log(error)
+    }else{
+        console.log(results);
+        Prediction_1=results[0].label;
+        Prediction_2=results[1].label;
+        Speak()
+    }
+    if(Prediction_1=="👋"){
+        document.getElementById("updateEmoji").innerHTML="👋";
+    }
+    if(Prediction_1=="👍"){
+        document.getElementById("updateEmoji").innerHTML="👍";
+    }
+
+    if(Prediction_1=="👌"){
+        document.getElementById("updateEmoji").innerHTML="👌";
+    }
+
+    if(Prediction_1=="👎"){
+        document.getElementById("updateEmoji").innerHTML="👎";
+    }
+
+    if(Prediction_1=="👊"){
+        document.getElementById("updateEmoji").innerHTML="👊";
+    }
+
+
+
+    if(Prediction_2=="👋"){
+        document.getElementById("updateEmoji2").innerHTML="👋";
+    }
+    if(Prediction_2=="👍"){
+        document.getElementById("updateEmoji2").innerHTML="👍";
+    }
+
+    if(Prediction_2=="👌"){
+        document.getElementById("updateEmoji2").innerHTML="👌";
+    }
+
+    if(Prediction_2=="👎"){
+        document.getElementById("updateEmoji2").innerHTML="👎";
+    }
+
+    if(Prediction_2=="👊"){
+        document.getElementById("updateEmoji2").innerHTML="👊";
+    }
 }
